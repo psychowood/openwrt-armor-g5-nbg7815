openwrt/openwrt

target/ipq807x: add fan support for ZyXEL NBG7815
This add simple support support for the fan on the NBG6715. It only allows
on/off. Wifi and NSS temperature are monitored.

This is based on the FAN control on mvebu.

Signed-off-by: André Valentin <avalentin@marcant.net>

target/linux/ipq807x/base-files/sbin/fan_ctrl.sh
@@ -1,0 +1,23 @@
+#!/bin/sh
+
+CPU_TEMP=$(cat /sys/class/thermal/thermal_zone0/temp)
+WIFI0_TEMP=$(cat /sys/class/hwmon/hwmon0/temp1_input 2>/dev/null) || WIFI0_TEMP=0
+WIFI1_TEMP=$(cat /sys/class/hwmon/hwmon1/temp1_input 2>/dev/null) || WIFI1_TEMP=0
+WIFI2_TEMP=$(cat /sys/class/hwmon/hwmon2/temp1_input 2>/dev/null) || WIFI2_TEMP=0
+
+CPU_LOW=65000
+CPU_HIGH=70000
+WIFI_LOW=65000
+WIFI_HIGH=70000
+
+if [ -d /sys/class/gpio/fan/value ];then
+	FAN_CTRL=/sys/class/gpio/fan/value
+else
+	exit 0
+fi
+
+if [ "$CPU_TEMP" -ge "$CPU_HIGH" -o "$WIFI0_TEMP" -ge "$WIFI0_HIGH" -o "$WIFI1_TEMP" -ge "$WIFI_HIGH" -o "$WIFI2_TEMP" -ge "$WIFI_HIGH" ];then
+	echo "1" > $FAN_CTRL
+elif [ "$CPU_TEMP" -lt "$CPU_LOW1" -o "$WIFI0_TEMP" -lt "$WIFI0_LOW" -o "$WIFI1_TEMP" -lt "$WIFI_LOW" -o "$WIFI2_TEMP" -lt "$WIFI_LOW" ];then
+	echo "0" > $FAN_CTRL
+fi

psychowood/openwrt-armor-g5-nbg7815

target/linux/ipq807x/base-files/sbin/fan_ctrl.sh
@@ -1,0 +1,22 @@
+#!/bin/sh
+
+CPU_TEMP=$(cat /sys/class/thermal/thermal_zone0/temp)
+WIFI0_TEMP=$(cat /sys/class/hwmon/hwmon0/temp1_input 2>/dev/null) || WIFI0_TEMP=0
+WIFI1_TEMP=$(cat /sys/class/hwmon/hwmon1/temp1_input 2>/dev/null) || WIFI1_TEMP=0
+WIFI2_TEMP=$(cat /sys/class/hwmon/hwmon2/temp1_input 2>/dev/null) || WIFI2_TEMP=0
+
+CPU_LOW=65000
+CPU_HIGH=70000
+WIFI_LOW=65000
+WIFI_HIGH=70000
+
+FAN_CTRL=/sys/class/gpio/fan/value
+if ! [ -f "$FAN_CTRL" ];then
+	exit 0
+fi
+
+if [ "$CPU_TEMP" -ge "$CPU_HIGH" -o "$WIFI0_TEMP" -ge "$WIFI_HIGH" -o "$WIFI1_TEMP" -ge "$WIFI_HIGH" -o "$WIFI2_TEMP" -ge "$WIFI_HIGH" ];then
+	echo "1" > $FAN_CTRL
+elif [ "$CPU_TEMP" -lt "$CPU_LOW" -o "$WIFI0_TEMP" -lt "$WIFI_LOW" -o "$WIFI1_TEMP" -lt "$WIFI_LOW" -o "$WIFI2_TEMP" -lt "$WIFI_LOW" ];then
+	echo "0" > $FAN_CTRL
+fi